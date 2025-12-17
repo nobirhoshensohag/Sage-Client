bc90e56fa7c645c21c9fea506b140b12a0e65c06
nobirhoshensohag <nobirhoshensohag708@gmail.com>
2025-12-18 01:59:27 +0600

    main url add

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
-import React from "react";
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "sage-server-rouge.vercel.app",
 });
 const useAxios = () => {
   return instance;
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "sage-server-rouge.vercel.app",
 });
 
 const useAxiosSecure = () => {
