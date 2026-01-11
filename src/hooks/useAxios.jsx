f2d15661ddc78f51b422b7435e22b2eb2450162d
nobirhoshensohag <nobirhoshensohag708@gmail.com>
2026-01-12 00:51:53 +0600

    add url linl

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://sage-server-rouge.vercel.app",
 });
 const useAxios = () => {
   return instance;
